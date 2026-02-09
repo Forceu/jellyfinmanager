07613fc891cf204c2c95059113c02290b8fb89dc
Marc Ole Bulling <Marc-Ole@gmx.de>
2026-02-09 21:20:26 +0100

    Added builder script, removed release.yml

---
+++ build/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+targets=${@-"darwin/amd64 darwin/arm64 linux/amd64 linux/386 linux/arm linux/arm64 linux/riscv64 windows/amd64 windows/arm64"}
+
+cd /usr/src/myapp
+go generate ./...
+
+for target in $targets; do
+	os="$(echo $target | cut -d '/' -f1)"
+	arch="$(echo $target | cut -d '/' -f2)"
+	output="build/jellyfinmanager-${os}_${arch}"
+	if [ $os = "windows" ]; then
+		output+='.exe'
+	fi
+
+	echo "----> Building jellyfinmanager for $target"
+	GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -tags docker -ldflags="-s -w" -o $output github.com/forceu/jellyfinmanager
+	zip -j $output.zip $output >/dev/null
+	rm $output
+done
+
+echo "----> Build is complete. List of files at build/:"
+cd build/
+ls -l jellyfinmanager-*
